1d5d5f4ad7ca213933156b0d875a7988f1cc92e5
Wandahsu <wandahsu1004@gmail.com>
2022-06-10 17:53:26 +0800

    Hello with person

--- src/main/java/com/wanda/Hello.java
+++ src/main/java/com/wanda/Hello.java
@@ -1,4 +1,9 @@
 package com.wanda;
 
 public class Hello {
+    public static void main(String[] args) {
+        //System.out.println("Hello world");
+        Person p = new Person();
+        p.greeting();
+    }
 }
--- src/main/java/com/wanda/Person.java
+++ src/main/java/com/wanda/Person.java
@@ -1,4 +1,7 @@
 package com.wanda;
 
 public class Person {
+    public void greeting() {
+        System.out.println("Good day!");
+    }
 }
