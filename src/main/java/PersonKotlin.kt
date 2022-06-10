15b7a6dfa3c881ed7204d9ed4981544d04c3d6aa
Wandahsu <wandahsu1004@gmail.com>
2022-06-10 17:52:42 +0800

    Initial Commit

---
+++ src/main/java/PersonKotlin.kt
@@ -0,0 +1,13 @@
+package com.wanda
+
+class PersonKotlin {
+    fun greeting() {
+        println("Hello kotlin")
+    }
+}
+
+//1st level
+fun main() {
+    val p = PersonKotlin()
+    p.greeting()
+}
---
+++ src/main/java/com/wanda/Person.java
@@ -0,0 +1,4 @@
+package com.wanda;
+
+public class Person {
+}
