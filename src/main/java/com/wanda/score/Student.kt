a9ab770e51896cabd95e1ed3286c2bb87f388176
Wandahsu <wandahsu1004@gmail.com>
2022-06-17 13:02:23 +0800

    Score test

---
+++ src/main/java/com/wanda/score/Student.kt
@@ -0,0 +1,24 @@
+package com.wanda.com.wanda.score
+
+class Student(var name:String,var english :Int,var math : Int) {
+    init {
+        println("Testing")
+    }
+    fun print() {
+        println("$name\t$english\t$math\t${getAverage()}")
+    }
+    fun getAverage() = (english+math)/2
+    /*
+    fun getAverage() : Int{
+        return(english+math)/2
+    }
+    */
+
+
+}
+
+fun main() {
+    val jack = Student("Jack",25,79)
+   //println(jack.math)
+    jack.print()
+}
